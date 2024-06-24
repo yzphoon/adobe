cb3b127008bd3521481c90703597568407eb6ae7
yzphoon <140886325+yzphoon@users.noreply.github.com>
2024-06-24 20:00:48 +0800

    add js function， defer js

--- js/master.js
+++ js/master.js
@@ -1,23 +1,49 @@
-const isMobile = $(window).width()<=480;
-const $grid = $('#Grid')
+const isMobile = $(window).width() <= 480;
+const $grid = $('#Grid');
+const $nav = $('#Nav')
 
-console.log ('isMobil = ' + isMobile);
+
+console.log('isMobil = ' + isMobile);
 
 function setGrid() {
     if (isMobile) {
         $grid.addClass('grid-2');
         return;
-    } 
+    }
     $grid.addClass('grid-3');
 }
 
-setGrid();
+function setScroll() {
+    $(window).scroll(function () {
+        if (isMobile) {
+            if ($(window).scrollTop() <= 20) {
+                $nav.removeClass('nav-active');
+                return;
+            }
+            $nav.addClass('nav-active');
+        }
+    });
+}
 
+function setInit() {
+    setGrid();
+    //其他載入畫面的函式庫
+}
 
-$(window).scroll(function () {
-    if ($(window).scrollTop() == 0) {
-        $('#Nav').removeClass('nav-active');
-        return;
-    }
-    $('#Nav').addClass('nav-active');
-});
+function setEvent() {
+    setScroll();
+    //其他事件的函式庫
+}
+
+setInit();
+setEvent();
+
+// $(window).scroll(function () {
+//     if (!isMobile) return;
+    
+//     if ($(this).scrollTop() <= 20) {
+//         $nav.removeClass('nav-active');
+//         return;
+//     }
+//     $nav.addClass('nav-active');
+// });
